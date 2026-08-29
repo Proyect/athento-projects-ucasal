fbe8e05d862cd350e86824a6dc76d68298c75810
ARIEL MARCELO DIAZ <amdiaz@ucasal.edu.ar>
2026-08-29 20:13:18 -0300

    add is_digit utility import to ApruebaTitulo workflow

--- operations/ucasal_aprobacion_titulo.py
+++ operations/ucasal_aprobacion_titulo.py
@@ -8,6 +8,7 @@ from core.exceptions import AthentoseError
 from django_currentuser.middleware import get_current_user
 from file.foperations import op_send_by_email
 from custom.ucasal2.external_services.ucasal.ucasal_services import UcasalServices
+from ucasal2.utils import is_digit
 
 
 class ApruebaTitulo(DocumentOperation):
