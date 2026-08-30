0019df11024c2a156592944a152a82f8415ee420
ARIEL MARCELO DIAZ <amdiaz@ucasal.edu.ar>
2026-08-29 21:19:45 -0300

    move is_digit validation back to utils from UcasalServices class in ApruebaTitulo workflow

--- operations/ucasal_aprobacion_titulo.py
+++ operations/ucasal_aprobacion_titulo.py
@@ -42,7 +42,7 @@ class ApruebaTitulo(DocumentOperation):
                 if otp_str == "":
                     flogger.entry("El OTP no puede ser nulo, ingrese un valor válido")
                     raise AthentoseError("El OTP no puede ser nulo, ingrese un valor válido")
-                if not UcasalServices.is_digit(otp_str):
+                if not is_digit(otp_str):
                     flogger.entry(f"'OTP' debe ser un número entero positivo en lugar de '{otp_str}'")
                     raise AthentoseError(
                         _("'OTP' debe ser un número entero positivo en lugar de '%(otp)s'")
