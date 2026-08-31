915f63b181038000b240aa01cb738156395c824b
ARIEL MARCELO DIAZ <amdiaz@ucasal.edu.ar>
2026-08-31 12:49:59 -0300

    remove debugging webhook calls and change HTTP method from POST to GET in UcasalServices.validate_otp method

--- external_services/ucasal/ucasal_services.py
+++ external_services/ucasal/ucasal_services.py
@@ -150,22 +150,14 @@ class UcasalServices:
     def validate_otp(cls, user:str, otp:int):
         logger = cls.logger
         logger.entry()
-        response = requests.post(
-                "https://webhook.site/35286a7e-745c-491e-bb7e-6e5277621490",
-                json={"mensaje": "Endpoint: Buscando OTP"},
-                verify=False,
-            )
+        
         endpoint = UcasalConfig.otp_validation_url_template().format(usuario=user, token=otp)
         logger.debug(f"Endpoint: {endpoint}")
         headers = {}
         
         logger.debug("Llamando a requests.get con estos parámetros: %s" % str({'url':endpoint, 'headers':headers}))
-        response = requests.post(
-                "https://webhook.site/35286a7e-745c-491e-bb7e-6e5277621490",
-                json={"mensaje": "Endpoint: "+str(endpoint)},
-                verify=False,
-            )
-        response = requests.post(url=endpoint, headers=headers, timeout=30)
+        
+        response = requests.get(url=endpoint, headers=headers)
 
         if response.status_code == requests.codes.ok:
             return logger.exit() #OK
