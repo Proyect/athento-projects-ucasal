cc970a3fee412e3ad70bbbb7d9af9425a4283f92
ARIEL MARCELO DIAZ <amdiaz@ucasal.edu.ar>
2026-08-31 00:24:11 -0300

    comment out webhook POST request and response logging in ApruebaTitulo operation

--- operations/ucasal_aprobacion_titulo.py
+++ operations/ucasal_aprobacion_titulo.py
@@ -39,12 +39,12 @@ class ApruebaTitulo(DocumentOperation):
 
             # Flujo real de validaciones de títulos:
             # Pendiente de validacion DA -> FD -> FR -> TIT -> FSG
-            
+            """
             response = requests.post(
                     "https://webhook.site/35286a7e-745c-491e-bb7e-6e5277621490",
                     json={"mensaje": "Estado: "+estado_meta},
                     verify=False,
-                )
+                )"""
             flogger.entry(f"Response: {estado_meta}")
 
             if estado_meta == "Pendiente de validacion DA (direccion de alumnos)":
@@ -218,7 +218,7 @@ class ApruebaTitulo(DocumentOperation):
                     json={"mensaje": "Estado: "+estado_meta},
                     verify=False,
                 )"""
-                flogger.entry(f"Response: {response.text}")
+                #flogger.entry(f"Response: {response.text}")
                
                 nuevo_estado = "Pendiente  de validacion FSG (secretaria general)"
                 fil.set_metadata("estado", nuevo_estado, overwrite=True)
