1a8777e0ed0dcb6e9329efb3ec6771b03a9e8e92
ARIEL MARCELO DIAZ <amdiaz@ucasal.edu.ar>
2026-08-31 00:13:49 -0300

    move webhook POST request outside of empty motivo check in RechazaTitulo operation

--- operations/ucasal_rechazo_titulo.py
+++ operations/ucasal_rechazo_titulo.py
@@ -42,15 +42,15 @@ class RechazaTitulo(DocumentOperation):
         try:
             
             # 2. Obtener motivo de rechazo
-            motivo = str(fil.gmv("metadata.form_titulo_motivo_de_rechazo") or "-")
+            motivo = fil.gmv("metadata.form_titulo_motivo_de_rechazo") or "-"
             motivo = str(motivo)
 
-            if (motivo == ""):
-                response = requests.post(
+            response = requests.post(
                     "https://webhook.site/35286a7e-745c-491e-bb7e-6e5277621490",
                     json={"mensaje": "Motivo: "+motivo},
                     verify=False,
                 )
+            if (motivo == ""):                
                 raise AthentoseError("Debe ingresar un motivo de rechazo para continuar.")
 
             # 3. Actualizar metadatos de rechazo / firma
