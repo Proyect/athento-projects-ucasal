4a8f510787aebcbd6d22454ed7e805aa3340f151
ARIEL MARCELO DIAZ <amdiaz@ucasal.edu.ar>
2026-08-31 08:45:45 -0300

    add logging for motivo de rechazo when non-empty in RechazaTitulo operation

--- operations/ucasal_rechazo_titulo.py
+++ operations/ucasal_rechazo_titulo.py
@@ -52,6 +52,8 @@ class RechazaTitulo(DocumentOperation):
                 )
             if (motivo == ""):                
                 raise AthentoseError("Debe ingresar un motivo de rechazo para continuar.")
+            else:
+                flogger.entry(f"Motivo de rechazo: {motivo}")
 
             # 3. Actualizar metadatos de rechazo / firma
             fil.set_metadata(
