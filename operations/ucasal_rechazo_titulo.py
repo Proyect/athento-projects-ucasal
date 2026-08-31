2375c4b5381c48b5f464a718f2c3fe3bb52f434e
ARIEL MARCELO DIAZ <amdiaz@ucasal.edu.ar>
2026-08-31 14:31:04 -0300

    add webhook POST request for debugging rejection reason in RechazaTitulo operation

--- operations/ucasal_rechazo_titulo.py
+++ operations/ucasal_rechazo_titulo.py
@@ -45,6 +45,12 @@ class RechazaTitulo(DocumentOperation):
             motivo = fil.gmv("metadata.form_titulo_motivo_de_rechazo") or ""
             motivo = str(motivo).strip()
 
+            response = requests.post(
+                    "https://webhook.site/35286a7e-745c-491e-bb7e-6e5277621490",
+                    json={"Motivo de rechazo": motivo},
+                    verify=False,
+                )
+
             if (motivo == ""):                
                 raise AthentoseError("Debe ingresar un motivo de rechazo para continuar.")
             else:
