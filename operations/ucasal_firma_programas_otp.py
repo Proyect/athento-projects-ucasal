7bdb7fdbc6bc3ecc8a884a0a6ab69bc1390de17e
ARIEL MARCELO DIAZ <amdiaz@ucasal.edu.ar>
2026-09-17 11:52:23 -0300

    update Docentes group validation conditional from if(False) to if(1 != 1) and add else branch with false condition log entry in FirmaProgramaOTP

--- operations/ucasal_firma_programas_otp.py
+++ operations/ucasal_firma_programas_otp.py
@@ -93,11 +93,13 @@ class FirmaProgramaOTP(DocumentOperation):
                 flogger.entry("No hay un usuario autenticado para firmar el programa")
                 raise AthentoseError("No hay un usuario autenticado para firmar el programa")
             
-            if(False):   #not usuario.groups.filter(name="Docentes").exists():
+            if(1 != 1):   #not usuario.groups.filter(name="Docentes").exists():
                 flogger.entry("El usuario logueado no pertenece al grupo 'Docentes'")
                 raise AthentoseError(
                     "El usuario logueado no pertenece al grupo 'Docentes'"
                 )
+            else:
+                flogger.entry("Entro por condicion falsa")
    
             mail_sg = usuario.email or ""
             nombre_sg = f"{usuario.first_name or ''} {usuario.last_name or ''}".strip()
