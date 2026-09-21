4f6cdc2726faaa8f18d615b58def4b3a7f9b24d0
ARIEL MARCELO DIAZ <amdiaz@ucasal.edu.ar>
2026-09-21 10:59:30 -0300

    comment out Docentes group validation check in FirmaProgramaOTP

--- operations/ucasal_firma_programas_otp.py
+++ operations/ucasal_firma_programas_otp.py
@@ -92,14 +92,14 @@ class FirmaProgramaOTP(DocumentOperation):
             if not usuario or not getattr(usuario, "is_authenticated", False):
                 flogger.entry("No hay un usuario autenticado para firmar el programa")
                 raise AthentoseError("No hay un usuario autenticado para firmar el programa")
-            
-            if not usuario.groups.filter(name="Docentes").exists():
+            """
+            if not usuario.groups.filter(name="Docentes").exists(): #not
                 flogger.entry("El usuario logueado no pertenece al grupo 'Docentes'")
                
                 #raise AthentoseError("El usuario logueado no pertenece al grupo 'Docentes'")
             else:
                 flogger.entry("Entro por condicion falsa")
-   
+            """
             mail_sg = usuario.email or ""
             nombre_sg = f"{usuario.first_name or ''} {usuario.last_name or ''}".strip()
             if not mail_sg:
